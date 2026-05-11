496547b93bf87e9609d03fb2bd18dc6716219cab
2017000375-CarlosPos <ca.pos.5cdb@gmail.com>
2026-05-10 19:19:03 -0600

    Add files via upload

---
+++ eje10guia4.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int autos;
+        int cajones = 200;
+
+        Console.WriteLine("--------------");
+        Console.WriteLine("ESTACIONAMIENTO");
+        Console.WriteLine("--------------");
+
+        while (cajones > 0)
+        {
+            Console.Write("Ingrese cantidad de autos: ");
+            autos = int.Parse(Console.ReadLine());
+
+            cajones = cajones - autos;
+
+            if (cajones > 0)
+            {
+                Console.WriteLine("Cajones disponibles: " + cajones);
+            }
+            else
+            {
+                Console.WriteLine("Ya no hay cajones disponibles");
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ eje9guia4.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int suma = 0;
+
+        Console.WriteLine("numeros primos:");
+
+        for (int num = 2; num <= 22; num++)
+        {
+            bool primo = true;
+
+            for (int divisor = 2; divisor < num; divisor++)
+            {
+                if (num % divisor == 0)
+                {
+                    primo = false;
+                }
+            }
+
+            if (primo == true)
+            {
+                Console.WriteLine(num);
+
+                suma = suma + num;
+            }
+        }
+
+        Console.WriteLine("Suma: " + suma);
+
+        Console.ReadKey();
+    }
+}
