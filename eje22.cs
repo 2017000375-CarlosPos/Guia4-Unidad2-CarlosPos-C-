81f7c731bd8a47af4d3fb33afac255d02b224471
2017000375-CarlosPos <ca.pos.5cdb@gmail.com>
2026-05-10 21:02:23 -0600

    Add files via upload

---
+++ eje22.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        Console.Write("Pon un numero: ");
+        num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Los primeros 20 múltiplos son:");
+        for (int i = 1; i <= 20; i++)
+        { 
+            Console.WriteLine(num * i);
+        }
+        Console.ReadKey();
+    }
+}
---
+++ eje24.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+        int sumaTot = 0; 
+
+        Console.Write("Pon un numero para la cuenta regresiva: ");
+        n = int.Parse(Console.ReadLine());
+
+        while (n >= 1)
+        {
+            Console.WriteLine(n);
+            sumaTot = sumaTot + n;
+            n--;
+        }
+
+        Console.WriteLine("La suma es: " + sumaTot);
+        Console.ReadKey();
+    }
+}
