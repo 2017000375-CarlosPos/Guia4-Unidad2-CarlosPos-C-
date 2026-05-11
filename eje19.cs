adaa2bac689ac4adb45abe3733b7eacfd5c882be
2017000375-CarlosPos <ca.pos.5cdb@gmail.com>
2026-05-10 20:49:33 -0600

    Add files via upload

---
+++ eje19.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        int suma = 0;
+        int pares = 0;
+        Console.WriteLine("Multiplos de 7");
+
+        for (int crup = 1; crup <= 20; crup++)
+        {
+            num = crup * 7;
+
+            Console.WriteLine(num);
+
+            suma = suma + num;
+
+            if (num % 2 == 0)
+            {
+                pares++;
+            }
+        }
+
+        Console.WriteLine("Suma total: " + suma);
+        Console.WriteLine("Cantidad de pares: " + pares);
+
+        Console.ReadKey();
+    }
+}
